4af883384903bd827a69595805472464e5d99d57
tanjimahmed14 <tanjim.cmt@gmail.com>
2024-02-01 22:53:32 +0600

    jwt push in the github

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,6 +4,23 @@ const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  axiosSecure.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('access-token')
+    console.log('request stoped by interceptors', token)
+    config.headers.authorization = `Bearer ${token}`
+    return config
+  }), function(error){
+    return Promise.reject(error)
+  }
+
+  // intercept 401 and 403
+  axiosSecure.interceptors.response.use(function(response){
+    return response
+  }, (error) => {
+    const status = error.request.status;
+    console.log('intercept error code:', status)
+    return Promise.reject(error)
+  })
   return axiosSecure;
 };
 
